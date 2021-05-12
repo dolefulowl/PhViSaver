1ccd0505e9a967b0a132c9a053c855e3a8ba37d4
dolefulowl <61901844+dolefulowl@users.noreply.github.com>
2021-05-12 21:37:11 +0300

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,9 +1,17 @@
-// closes your tab if 's' has been pressed
-function closeTab(e) {
-    let key = e.code;
-    if(key === 'KeyS') {
-        chrome.runtime.sendMessage({close: "theTab"});
-    }
+function closeTab(event) {
+    let currentKey = event.code;
+    let theKey;
 
+    // get theKey from the local storage
+    chrome.storage.local.get('theKey', function(result){
+        theKey = result.theKey;
+        // if it's the first run, set the default value for theKey
+        if(!theKey) {
+            chrome.storage.local.set({'theKey': ['Digit3', '3']});
+        }
+        if(currentKey === theKey[0]) {
+            chrome.runtime.sendMessage({close: "theTab"});
+        }
+    });
 }
 document.addEventListener('keypress', closeTab);
